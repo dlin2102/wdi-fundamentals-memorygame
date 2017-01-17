f89f37536734810176874af2bd64be3befc28926
Dan Lin <dlin2102@gmail.com>
2017-01-16 21:13:16 -0500

    unit 10

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,10 +1,24 @@
 console.log("JS file is connected to HTML! Woo!")
+
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
 if (cardFour === cardTwo){
 	alert('You found a match!')
-}else {
+} else {
 	alert('Sorry, try again.')
 }
+
+var gameBoard = document.getElementById("game-board");
+
+function createCards() {
+	for (var i = 0; i < 4; i++) {
+		var card = document.createElement('div')
+		card.className = 'card';
+		gameBoard.appendChild(card);
+	}
+}
+
+createCards();
